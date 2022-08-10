4799476b09c4f34f271c9dfc3097413bf248ef26
Mustafa Poya <mustafapoyaabc@gmail.com>
2022-08-10 17:15:49 +0430

    create controller of find view

---
+++ src/net/golbarg/findday/FindViewController.java
@@ -0,0 +1,25 @@
+package net.golbarg.findday;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class FindViewController {
+	@FXML private Label lblDay;
+	@FXML private Label lblMonth;
+	@FXML private Label lblYear;
+
+	@FXML private TextField txtDay;
+	@FXML private TextField txtMonth;
+	@FXML private TextField txtyear;
+
+	@FXML private Button btnFindDay;
+
+	@FXML private Label lblResult;
+	
+	@FXML 
+	private void initialize() {
+		
+	}
+}
